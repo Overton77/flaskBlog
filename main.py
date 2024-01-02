a3d15735af58da9af6ea4003170a2675c81f4708
Overton77 <Johnoverton743@gmail.com>
2024-01-01 20:25:22 -0500

    inputed environment variables adding Procfile

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 # Import your forms from the forms.py
-from forms import CreatePostForm, CommentForm, LoginForm, UserForm 
+from forms import CreatePostForm, CommentForm, LoginForm, UserForm  
+import os
 from dotenv import load_dotenv 
 load_dotenv()
 
@@ -30,7 +31,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SECRET_KEY"] = os.environ.get("FLASK_KEY")
 ckeditor = CKEditor(app)
 bootstrap =  Bootstrap5(app) 
 
@@ -42,7 +43,7 @@ bootstrap =  Bootstrap5(app)
 
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy()
 db.init_app(app)  
 login_manager = LoginManager() 
@@ -318,4 +319,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False, port=5002)
